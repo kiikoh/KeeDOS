37dea46cb713c53a358c8d5358d81846359925b1
Alan Labouseur <Alan.Labouseur@Marist.edu>
2022-08-28 18:48:02 -0400

    (finally) fixes location.reload() for new version of TypeScript

--- source/host/control.ts
+++ source/host/control.ts
@@ -107,7 +107,7 @@ module TSOS {
 
         public static hostBtnReset_click(btn): void {
             // The easiest and most thorough way to do this is to reload (not refresh) the document.
-            location.reload(true);
+            location.reload();
             // That boolean parameter is the 'forceget' flag. When it is true it causes the page to always
             // be reloaded from the server. If it is false or not specified the browser may reload the
             // page from its cache, which is not what we want.
