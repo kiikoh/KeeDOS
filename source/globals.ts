806527140e77352061c060c933cd628ecb645db6
kiikoh <ikikomonkkey@gmail.com>
2022-09-05 10:43:39 -0400

    change the ver command #1

--- source/globals.ts
+++ source/globals.ts
@@ -11,8 +11,8 @@
 //
 // Global CONSTANTS (TypeScript 1.5 introduced const. Very cool.)
 //
-const APP_NAME: string    = "TSOS";   // 'cause Bob and I were at a loss for a better name.
-const APP_VERSION: string = "0.07";   // What did you expect?
+const APP_NAME: string    = "KeeDOS";   // 'cause Bob and I were at a loss for a better name.
+const APP_VERSION: string = "0.1";   // What did you expect?
 
 const CPU_CLOCK_INTERVAL: number = 100;   // This is in ms (milliseconds) so 1000 = 1 second.
 
