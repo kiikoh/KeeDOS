ecbc61501a2afe487d96c8f6c6eef388773e91ee
kiikoh <ikikomonkey@gmail.com>
2022-09-14 12:05:47 -0400

    version bump

--- source/globals.ts
+++ source/globals.ts
@@ -12,7 +12,7 @@
 // Global CONSTANTS (TypeScript 1.5 introduced const. Very cool.)
 //
 const APP_NAME: string    = "KeeDOS";   // 'cause Bob and I were at a loss for a better name.
-const APP_VERSION: string = "0.1";   // What did you expect?
+const APP_VERSION: string = "0.2";   // What did you expect?
 
 const CPU_CLOCK_INTERVAL: number = 100;   // This is in ms (milliseconds) so 1000 = 1 second.
 
